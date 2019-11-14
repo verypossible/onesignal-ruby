a6e05ed22f501a2f0a4d069266fcf9eec1b2bf6c
Tomas Herrera <tomashbarros@gmail.com>
2019-11-14 14:02:54 -0500

    Add support for large and small icons on notifications

--- lib/onesignal/sounds.rb
+++ lib/onesignal/sounds.rb
@@ -2,15 +2,17 @@
 
 module OneSignal
   class Sounds
-    attr_reader :ios, :android, :amazon, :windows
+    attr_reader :ios, :android, :amazon, :windows, :small_icon
 
-    def initialize ios: nil, android: nil, amazon: nil, windows: nil
+    def initialize ios: nil, android: nil, amazon: nil, windows: nil, small_icon: nil, large_icon: nil
       validate ios: ios, windows: windows
 
       @ios = ios
       @android = android
       @amazon = amazon
       @windows = windows
+      @small_icon = small_icon
+      @large_icon = large_icon
     end
 
     def as_json options = nil
@@ -18,7 +20,9 @@ module OneSignal
         'ios_sound'     => @ios.as_json(options),
         'android_sound' => @android.as_json(options),
         'adm_sound'     => @amazon.as_json(options),
-        'wp_wns_sound'  => @windows.as_json(options)
+        'wp_wns_sound'  => @windows.as_json(options),
+        'small_icon'    => @small_icon.as_json(options),
+        'large_icon'    => @large_icon.as_json(options)
       }
     end
 
